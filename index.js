e91e13b14a3edc12d519cb430f06c8163c6b1e15
Kunal Chauhan <kunalchauhanlion@gmail.com>
2023-01-16 22:29:19 +0530

    done with a cleaner code

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ jokebtn.addEventListener('click',genratejoke)
 
 genratejoke()
 
-function genratejoke(){
+async function genratejoke(){
     const config = {
         headers : {
             Accept : 'application/json',
         },
-    } 
-    fetch('https://icanhazdadjoke.com',config)
-    .then((res)=>res.json())
-    .then((data)=>{
-        jokel.innerHTML = data.joke;
-    })
+    }
+    const res = await fetch('https://icanhazdadjoke.com',config);
+    const data = await res.json();
+    jokel.innerHTML = data.joke;
+    
+    //fetch('https://icanhazdadjoke.com',config)
+    //.then((res)=>res.json())
+    //.then((data)=>{
+    //    jokel.innerHTML = data.joke;
+    //})
 }
